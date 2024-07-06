6b6e20f2d018ab393bf8d8b2b29fbf3c832f2e35
gianmalarcon <gian@quantum3labs.com>
2024-07-06 15:54:46 +0700

    Feat: Add demo status

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ pub async fn main() {
     // Build our application by creating our router.
     let app = axum::Router::new()
         .fallback(fallback)
-        .route("/", get(hello));
+        .route("/", get(hello))
+        .route("/demo-status", get(demo_status));
 
     // Run our application as a hyper server on http://localhost:3000.
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -26,4 +27,10 @@ pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse
         axum::http::StatusCode::NOT_FOUND,
         format!("No route {}", uri),
     )
+}
+
+/// axum handler for "GET /demo-status" which returns a HTTP status
+/// code, such as OK (200), and a custom user-visible string message.
+pub async fn demo_status() -> (axum::http::StatusCode, String) {
+    (axum::http::StatusCode::OK, "Everything is OK".to_string())
 }

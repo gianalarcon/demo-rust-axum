128b9d36bd80c9dc64e487e4f54e8c8b676236df
gianmalarcon <gian@quantum3labs.com>
2024-07-06 13:40:07 +0700

    Feat: Add fallback

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use axum::routing::get;
 #[tokio::main]
 pub async fn main() {
     // Build our application by creating our router.
-    let app = axum::Router::new().route("/", get(hello));
+    let app = axum::Router::new()
+        .fallback(fallback)
+        .route("/", get(hello));
 
     // Run our application as a hyper server on http://localhost:3000.
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -15,4 +17,13 @@ pub async fn main() {
 /// immediately respond with status code `200 OK` and with the string.
 pub async fn hello() -> String {
     "Hello, World".to_string()
+}
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
 }

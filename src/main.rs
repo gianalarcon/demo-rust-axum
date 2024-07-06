f7af4b2815a9111926e0d10e29abd9519626f6d1
gianmalarcon <gian@quantum3labs.com>
2024-07-06 16:19:11 +0700

    Feat: Read from .env file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-/// Use axum capabilities.
 use axum::routing::get;
-
+/// Use axum capabilities.
+use dotenv::dotenv;
+use std::env;
 #[tokio::main]
 pub async fn main() {
     // Build our application by creating our router.
     let app = axum::Router::new()
         .fallback(fallback)
         .route("/", get(hello))
-        .route("/demo-status", get(demo_status));
+        .route("/read-env", get(read_env));
 
     // Run our application as a hyper server on http://localhost:3000.
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -29,8 +30,10 @@ pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse
     )
 }
 
-/// axum handler for "GET /demo-status" which returns a HTTP status
-/// code, such as OK (200), and a custom user-visible string message.
-pub async fn demo_status() -> (axum::http::StatusCode, String) {
-    (axum::http::StatusCode::OK, "Everything is OK".to_string())
+/// axum handler for "GET /read-env" which returns a HTTP status
+/// code, such as OK (200), and a custom string read from the environment.
+pub async fn read_env() -> (axum::http::StatusCode, String) {
+    dotenv().ok();
+    let text = env::var("TEXT").expect("TEXT must be set");
+    (axum::http::StatusCode::OK, text)
 }
